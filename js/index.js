b436708c4bd71ccfad984881e4b6fd8643d3f8cd
coding655 <codingmaster655@gmail.com>
2024-01-24 10:35:45 +0600

    commit by coding655

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log('Coding655');
+function getInputValue(inputId){
+
+    const inputFieldId = document.getElementById(inputId);
+    const inputFieldInnerText = inputFieldId.innerText;
+    const inputFieldString = parseFloat(inputFieldInnerText);
+    return inputFieldString;
+
+} 
+
+function setTheValueById(inputId, elementId){
+
+    const elementField = document.getElementById(inputId);
+    elementField.innerText = elementId;
+}
+function OnClickHandler(){
+    const firstProductId = getInputValue('product-1');
+    const Discount = firstProductId * 5/100;
+    setTheValueById('total-items',firstProductId,Discount, 'discount-id');
+    // setTheValueById(Discount, 'discount-id');
+}
+
